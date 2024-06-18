95040af3b24f0c412ae67a53067fbbd371a58d3f
data-miner00 <data-miner00@users.noreply.github.com>
2024-06-19 00:16:45 +0800

    docs: Add generator

---
+++ docs/generator.js
@@ -0,0 +1,37 @@
+function* range(start, end) {
+  for (var i = start; i < end; i++) {
+    yield i;
+  }
+}
+
+for (var num of range(5, 13)) {
+  console.log(num);
+}
+
+function* counterGenerator() {
+  var count = 0;
+
+  while (true) {
+    count = yield ++count;
+  }
+}
+
+var counter = counterGenerator();
+
+console.log(counter.next()); // 1
+console.log(counter.next(10)); // 11
+console.log(counter.next(15)); // 16
+
+function* fibonacciGenerator() {
+  var [prev, curr] = [0, 1];
+  while (true) {
+    yield curr;
+    [prev, curr] = [curr, prev + curr];
+  }
+}
+
+var fibonacci = fibonacciGenerator();
+
+for (var i = 0; i < 10; i++) {
+  console.log(fibonacci.next());
+}
